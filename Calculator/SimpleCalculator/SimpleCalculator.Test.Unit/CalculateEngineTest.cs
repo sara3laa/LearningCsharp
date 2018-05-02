da761d9b5718d37c3c90061e0f589d4e028c78dc
sara3laa <sara3laa.93@gmail.com>
2018-05-02 17:20:31 +0200

    It's the Unit Testing Of the simple Calculator basicly we create two test one for input converter and the other to calculate but basicly we check the add operation

---
+++ Calculator/SimpleCalculator/SimpleCalculator.Test.Unit/CalculateEngineTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCalculator.Test.Unit
+{
+    [TestClass]
+    public class CalculateEngineTest
+    {
+        private  readonly CalculateEngine _calculateEngine  = new CalculateEngine();
+        [TestMethod]
+        public void AddsTwoNumbersAndReturnsValidResultForNonSymbolOperation()
+        {
+            int num1 = 1;
+            int num2 = 2;
+            double result = _calculateEngine.Calculate("add", num1, num2);
+            Assert.AreEqual(3,result);
+
+        }
+        [TestMethod]
+        public void AddsTwoNumbersAndReturnsValidResultForSymbolOperation()
+        {
+            int num1 = 1;
+            int num2 = 2;
+            double result = _calculateEngine.Calculate("+", num1, num2);
+            Assert.AreEqual(3, result);
+
+        }
+    }
+}
---
+++ Calculator/SimpleCalculator/SimpleCalculator.Test.Unit/InputConverterTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCalculator.Test.Unit
+{
+    [TestClass]
+    public class InputConverterTest
+    {
+        private  readonly  InputConverter _inputConverter = new InputConverter();
+        [TestMethod]
+        public void ConvertValidStringInputIntoDouble()
+        {
+            var inputNum = "10";
+            var convertedNum = _inputConverter.ConvertToNumeric(inputNum);
+            Assert.AreEqual(10,convertedNum);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FaildToConvertInvalidStringInputIntoDouble()
+        {
+            var inputNum = "*";
+            var convertedNum = _inputConverter.ConvertToNumeric(inputNum);
+           
+        }
+    }
+}
