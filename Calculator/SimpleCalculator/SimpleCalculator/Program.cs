46f037493fd5d1e4a9e26e6b118f7a5a75344ba2
sara3laa <sara3laa.93@gmail.com>
2018-05-02 18:23:26 +0200

    The calculator program updated to accept more than two numbers like a real calculator

--- Calculator/SimpleCalculator/SimpleCalculator/Program.cs
+++ Calculator/SimpleCalculator/SimpleCalculator/Program.cs
@@ -13,12 +13,41 @@ namespace SimpleCalculator
             {
                 var inputconverter = new InputConverter();
                 var caclulator = new CalculateEngine();
+                Console.WriteLine("Enter a number");
                 double firstnumber = inputconverter.ConvertToNumeric(Console.ReadLine());
-                double secondnumber = inputconverter.ConvertToNumeric(Console.ReadLine());
+                Console.WriteLine("Enter an operator");
                 string operation = Console.ReadLine();
+                Console.WriteLine("Enter a number");
+                double secondnumber = inputconverter.ConvertToNumeric(Console.ReadLine());
                 double result = caclulator.Calculate(operation, firstnumber, secondnumber);
 
-                Console.WriteLine(result);
+                ConsoleKeyInfo cki;
+
+                do
+                {
+                    
+                    firstnumber = result;
+                    Console.WriteLine("\nEnter an operator");
+                    cki = Console.ReadKey();
+                  
+                    if (cki.Key == ConsoleKey.OemPlus)
+                    {
+                        break;
+                        
+                    }
+                    Console.WriteLine("\nEnter a number");
+                    secondnumber = inputconverter.ConvertToNumeric(Console.ReadLine());
+                    
+                    operation = Convert.ToString(cki.Key);
+                   
+                    result = caclulator.Calculate(operation, result, secondnumber);
+
+
+                } while (true);
+                
+                    
+               
+                Console.WriteLine(" "+result);
             }
             catch (Exception ex)
             {
