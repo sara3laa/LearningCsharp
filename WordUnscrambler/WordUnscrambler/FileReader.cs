32b243a47ce850e844c926adbe0d057b6cd0c07a
sara3laa <sara3laa.93@gmail.com>
2018-05-02 20:57:03 +0200

    FileReader Class to Read the file of words

---
+++ WordUnscrambler/WordUnscrambler/FileReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordUnscrambler
+{
+   public class FileReader
+    {
+        public string[] Read(string filename)
+        {
+            try
+            {
+                return File.ReadAllLines(filename);
+
+            }
+            catch (Exception e)
+            {
+               
+                throw new Exception(e.Message);
+            }
+            
+            
+
+        }
+    }
+}
