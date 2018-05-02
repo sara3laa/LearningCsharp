13b7a313118d59d01c1b9f84ad76539446e7d159
sara3laa <sara3laa.93@gmail.com>
2018-05-02 21:17:50 +0200

    this struct is about the word and it's scrambled

---
+++ WordUnscrambler/WordUnscrambler/MatchedWord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordUnscrambler
+{
+  public struct MatchedWord
+    {
+        public string ScrambledWord { get; set; }
+        public string Word { get; set; }
+    }
+}
