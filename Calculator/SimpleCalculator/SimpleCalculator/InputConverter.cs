957e58f32efb1d048bd3ae06a3d70f8c1f398007
sara3laa <sara3laa.93@gmail.com>
2018-05-02 16:34:30 +0200

    In this class we Convert the string input to numeric

---
+++ Calculator/SimpleCalculator/SimpleCalculator/InputConverter.cs
@@ -0,0 +1,23 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+  public class InputConverter
+    {
+        public double ConvertToNumeric(string Input)
+
+        {
+          
+            if (!double.TryParse(Input, out var number))
+            {
+                throw  new ArgumentException("The input is not Numeric");
+            }
+
+            return number;
+        }
+    }
+}
