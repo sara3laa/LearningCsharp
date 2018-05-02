255fdb14aa06277e05eab878f04253558292655d
sara3laa <sara3laa.93@gmail.com>
2018-05-02 16:35:51 +0200

    the first program using to the basic  calculator

--- Calculator/SimpleCalculator/SimpleCalculator/Program.cs
+++ Calculator/SimpleCalculator/SimpleCalculator/Program.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace SimpleCalculator
 {
     class Program
     {
         static void Main(string[] args)
         {
+            try
+            {
+                var inputconverter = new InputConverter();
+                var caclulator = new CalculateEngine();
+                double firstnumber = inputconverter.ConvertToNumeric(Console.ReadLine());
+                double secondnumber = inputconverter.ConvertToNumeric(Console.ReadLine());
+                string operation = Console.ReadLine();
+                double result = caclulator.Calculate(operation, firstnumber, secondnumber);
+
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                //TODO:strat logging Exception
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
